4e966de48d5a48653cee49474828b17d64118563
calvwng <calvwng@gmail.com>
2015-07-01 01:33:50 -0700

    Testing Tumbax Tumblr feed widget

---
+++ js/init_tumbax.js
@@ -0,0 +1,12 @@
+/**
+* Initialize the Tumbax Tumblr feed widget
+*/
+
+ //mandatory----------------
+ var tumblrBlogLink="http://calvwng.tumblr.com"; 
+ var tumblrApiKey="D1yio7eUVayjZlpcnygLnmIFyGAyqgvzRVSmLyuVVpOmt1Tg4q";
+  
+ //optional-----------------
+ var tumbaxWidgetWidth = 800; 
+ // var pathToLoadingImage = './loader.gif';
+ var tumbaxColumns = 2;
